7fc787c1172c70fabdc4a6fcf96ffbb5116df4ed
Lilia <lilia.manukyan@hotmail.com>
2021-02-24 22:10:02 +0400

    react starter app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Hello, React! </h1>
+    </div>
+  );
+};
+
+export default hot(module)(App);
