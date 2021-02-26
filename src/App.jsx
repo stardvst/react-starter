823579b937b5f05c48dc799f74238809429f7720
Subho Ghosh <subhoghs@protonmail.com>
2021-02-26 23:55:04 +0530

    Make tailwind work

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="app">
       <h1> Hello, React! </h1>
+      <h2 className="font-bold text-red-500">Is tailwind working?</h2>
+      <p className="text-4xl">I guess it's working</p>
     </div>
   );
 };
